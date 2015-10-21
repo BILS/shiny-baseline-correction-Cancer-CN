52c78f92965e00fcbdca44a2407bed50b018576d
Jonas Hagberg <jonas.hagberg@bils.se>
2015-10-21 10:57:13 +0200

    Initial commit

---
+++ server.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+  output$contents <- renderTable({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, it will be a data frame with 'name',
+    # 'size', 'type', and 'datapath' columns. The 'datapath'
+    # column will contain the local filenames where the data can
+    # be found.
+    regions <- input$file1
+    sampleList <- input$file2
+    
+
+    if (is.null(regions))
+      return(NULL)
+    
+    read.csv(regions$datapath, header=input$header, sep=input$sep, 
+				 quote=input$quote)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,54 @@
+library(shiny)
+library(markdown)
+
+shinyUI(navbarPage("Baseline Correction for Copy Number Data from Cancer Samples",
+  tabPanel("Description",
+   fluidPage(
+   titlePanel("Description"),
+   fluidRow(
+     column(5,
+     includeMarkdown("description.md")
+         ) 
+   )  )
+
+  ), 
+  
+  tabPanel("Uploading Files",
+  fluidPage(
+  titlePanel("Uploading Files"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('file1', 'Choose Regions CSV File',
+                accept=c('text/csv', 
+								 'text/comma-separated-values,text/plain', 
+								 '.csv')),
+       fileInput('file2', '*Optional* Choose samplenamne  CSV File',
+                accept=c('text/csv', 
+								 'text/comma-separated-values,text/plain', 
+								 '.csv')),
+      tags$hr(),
+      	HTML('<b>CSV file manipulate'),
+      checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', 'Separator',
+                   c(Comma=',',
+                     Semicolon=';',
+                     Tab='\t'),
+                   ','),
+      radioButtons('quote', 'Quote',
+                   c(None='',
+                     'Double Quote'='"',
+                     'Single Quote'="'"),
+                   '"')
+    ),
+    mainPanel(
+      tableOutput('contents')
+    )
+  )
+)),
+  tabPanel("Component 2"),
+  tabPanel("Component 3")
+
+
+
+
+))
