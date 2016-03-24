f67f9b0016cb13726ecf19b7fe09fb8014dd8fe6
Johan Viklund <johan.viklund@bils.se>
2016-03-24 09:53:58 +0100

    [server.R] Vectorised for loop

--- server.R
+++ server.R
@@ -346,20 +346,11 @@ shinyServer(function(input, output, session) {
         myPlot
     }
 
-
-    ### This function feels unidiomatic
     replaceChr <- function(object){
-        chromosome1 <- c( "chr1",  "chr2",  "chr3",  "chr4",  "chr5",  "chr6",  "chr7", "chr8",
-                          "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15",
-                         "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY")
-
-        chromosome2 <- c(    1,    2,    3,    4,    5,    6,    7,    8,    9,
-                         10,    11,    12,    13,    14,    15,    16,    17,
-                         18,    19,    20,    21,    22,    "X",    "Y"    )	
-
-        for (i in 1:24){
-            object$Chromosome[object$Chromosome==chromosome2[i]] <- chromosome1[i]
-        }
+        object$Chromosome <-
+             ifelse( grepl('^chr', object$Chromosome),    # If the name start with chr
+                     object$Chromosome,                   # It's the right name
+                     paste('chr', t1$Chromosome, sep="")) # Else prepend 'chr'
 
         invisible(object)
     }
